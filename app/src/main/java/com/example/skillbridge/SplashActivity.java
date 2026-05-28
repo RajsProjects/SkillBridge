74a4b33bda4e295d1a9916540367cea43149a75b
sachinsingh47 <singhsachinrathore3@gmail.com>
2026-05-28 22:26:26 +0530

    Added my side of the SkillBridge code

---
+++ app/src/main/java/com/example/skillbridge/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.skillbridge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        setContentView(R.layout.activity_splash);
+
+        WindowInsetsControllerCompat windowInsetsController = WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        windowInsetsController.setAppearanceLightStatusBars(false);
+        windowInsetsController.setAppearanceLightNavigationBars(false);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, SplashActivity.class);
+            startActivity(intent);
+            finish();
+        }, 5000);
+    }
+}
